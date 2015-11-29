8948acd72b3d6af57ddd5cde9ce02adc466c1ee3
Tomasz Balcerzak <tomasz.balcerzak@outlook.com>
2015-11-29 13:43:50 +0100

    Finished sha256 with file support

--- src/SHA2.jl
+++ src/SHA2.jl
@@ -1,19 +1,19 @@
 
 module SHA2
 
-export SHA256, compute
+export SHA256, compute!
 
-macro SHA256BLOCKSIZE()
+macro sha256blocksize()
     const block::UInt32 = (512/8)
     return :($block)
 end
-macro DIGESTSIZE()
+macro digestsize()
     const size::UInt32 = (256/8)
     return :($size)
 end
 
-macro SHA256K(idx)
-    const sha256k::Array{Int64} = 
+macro sha256k(idx)
+    const sha256karr::Array{Int64} = 
         [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
         0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
@@ -30,18 +30,18 @@ macro SHA256K(idx)
         0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
         0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
-    return :($sha256k[$idx])
+    return :($sha256karr[$idx])
 end
 
 type SHA256
     mtotlen::UInt32
     mlen::UInt32
-    mblock::Array{UInt8}
+    mblock::AbstractArray{UInt8}
     mh::Array{UInt32}
 
     message::Array{UInt8}
     
-    SHA256(message::ASCIIString)=begin
+    SHA256(message::ASCIIString; file::Bool=false)=begin
         local mh::Array{UInt32}=zeros(UInt8,8)
         mh[1] = 0x6a09e667
         mh[2] = 0xbb67ae85
@@ -53,10 +53,10 @@ type SHA256
         mh[8] = 0x5be0cd19
         
         local buffer::Array{UInt8}
-        if isfile(message)
-            file::IOStream = open(message)
-            content::ASCIIString=readall(file)
-            close(file)
+        if file && isfile(message)
+            f::IOStream = open(message)
+            content::ASCIIString=readall(f)
+            close(f)
             buffer=Vector{UInt8}(content)
         else
             buffer=Vector{UInt8}(message)
@@ -65,17 +65,17 @@ type SHA256
     end
 end
 
-SHA2_SHFR(x, n) = (x >> n)
-SHA2_ROTR(x, n) = ((x >> n) | (x << ((sizeof(x) << 3) - n)))
-SHA2_ROTL(x, n) = ((x << n) | (x >> ((sizeof(x) << 3) - n)))
-SHA2_CH(x, y, z) = ((x & y) $ (~x & z))
-SHA2_MAJ(x, y, z) = ((x & y) $ (x & z) $ (y & z))
-SHA256_F1(x) = (SHA2_ROTR(x,  2) $ SHA2_ROTR(x, 13) $ SHA2_ROTR(x, 22))
-SHA256_F2(x) = (SHA2_ROTR(x,  6) $ SHA2_ROTR(x, 11) $ SHA2_ROTR(x, 25))
-SHA256_F3(x) = (SHA2_ROTR(x,  7) $ SHA2_ROTR(x, 18) $ SHA2_SHFR(x,  3))
-SHA256_F4(x) = (SHA2_ROTR(x, 17) $ SHA2_ROTR(x, 19) $ SHA2_SHFR(x, 10))
+sha2shfr(x, n) = (x >> n)
+sha2rotr(x, n) = ((x >> n) | (x << ((sizeof(x) << 3) - n)))
+sha2rotl(x, n) = ((x << n) | (x >> ((sizeof(x) << 3) - n)))
+sha2ch(x, y, z) = ((x & y) $ (~x & z))
+sha2maj(x, y, z) = ((x & y) $ (x & z) $ (y & z))
+sha256f1(x) = (sha2rotr(x,  2) $ sha2rotr(x, 13) $ sha2rotr(x, 22))
+sha256f2(x) = (sha2rotr(x,  6) $ sha2rotr(x, 11) $ sha2rotr(x, 25))
+sha256f3(x) = (sha2rotr(x,  7) $ sha2rotr(x, 18) $ sha2shfr(x,  3))
+sha256f4(x) = (sha2rotr(x, 17) $ sha2rotr(x, 19) $ sha2shfr(x, 10))
 
-function SHA2_PACK32(str::AbstractArray{UInt8}, x::AbstractArray{UInt32})
+function sha2pack32!(str::AbstractArray{UInt8}, x::AbstractArray{UInt32})
     local val::UInt32 = 0
     try
         val |= (str[4])
@@ -90,43 +90,43 @@ function SHA2_PACK32(str::AbstractArray{UInt8}, x::AbstractArray{UInt32})
     x[1]=val
 end
 
-function SHA2_UNPACK32(x::AbstractArray{UInt32}, str::AbstractArray{UInt8})
+function sha2unpack32!(x::UInt32, str::AbstractArray{UInt8})
     try
-        str[4] |= UInt8(x[1] & 0xff)
+        str[4] |= UInt8(x & 0xff)
     end
     try
-        str[3] |= UInt8((x[1] >> 8) & 0xff)
+        str[3] |= UInt8((x >> 8) & 0xff)
     end
     try
-        str[2] |= UInt8((x[1] >> 16) & 0xff)
+        str[2] |= UInt8((x >> 16) & 0xff)
     end
-    str[1] |= UInt8(x[1] >> 24)
+    str[1] |= UInt8(x >> 24)
 end
 
-function transform(message::Array{UInt8}, block_nb::UInt32, mh::Array{UInt32})
-    w::Array{UInt32}=zeros(UInt32, 64)
-    wv::Array{UInt32}=zeros(UInt32, 8)
-    t1::UInt32=0
-    t2::UInt32=0
-    sub_block::Array{UInt8}=zeros(UInt8)
-    i::Int32=0
-    j::Int32=0
-    for i in 1:1:block_nb
-        sub_block = message[((i-1)<< 6)+1:end]
+function transform!(message::Array{UInt8}, blocknb::UInt32, mh::Array{UInt32})
+    local w::Array{UInt32}=zeros(UInt32, 64)
+    local wv::Array{UInt32}=zeros(UInt32, 8)
+    local t1::UInt32=0
+    local t2::UInt32=0
+    local subblock::AbstractArray{UInt8}=zeros(UInt8)
+    local i::Int32=0
+    local j::Int32=0
+    for i in 1:1:blocknb
+        subblock = sub(message, ((i-1)<< 6)+1:length(message))
         for j in 1:1:16
             try
-                SHA2_PACK32(sub(sub_block, ((j-1)<<2)+1:((j-1)<<2)+1), sub(w, j:j))
+                sha2pack32!(sub(subblock, ((j-1)<<2)+1:((j-1)<<2)+1), sub(w, j:j))
             end
         end
         for j in 17:1:64
-            w[j] =  SHA256_F4(w[j -  2]) + w[j -  7] + SHA256_F3(w[j - 15]) + w[j - 16]
+            w[j] =  sha256f4(w[j -  2]) + w[j -  7] + sha256f3(w[j - 15]) + w[j - 16]
         end
         for j in 1:1:8
             wv[j] = mh[j]
         end
         for j in 1:1:64
-            t1 = UInt64(wv[8] + SHA256_F2(wv[5]) + SHA2_CH(wv[5], wv[6], wv[7]) + @SHA256K(j) + w[j])%(typemax(UInt32)+1)
-            t2 = SHA256_F1(wv[1]) + SHA2_MAJ(wv[1], wv[2], wv[3])
+            t1 = UInt64(wv[8] + sha256f2(wv[5]) + sha2ch(wv[5], wv[6], wv[7]) + @sha256k(j) + w[j])%(typemax(UInt32)+1)
+            t2 = sha256f1(wv[1]) + sha2maj(wv[1], wv[2], wv[3])
             wv[8] = wv[7]
             wv[7] = wv[6]
             wv[6] = wv[5]
@@ -142,58 +142,60 @@ function transform(message::Array{UInt8}, block_nb::UInt32, mh::Array{UInt32})
     end
 end
 
-function final(ctx::SHA256, digest::Array{UInt8})
-    block_nb::UInt32=0
-    m_block::Array{UInt8}=ctx.mblock
-    pm_len::UInt32=0
-    len_b::UInt32=0
-    i::Int32=0
-    block_nb = (1 + ((@SHA256BLOCKSIZE() - 9) < (m_len % @SHA256BLOCKSIZE())))
-    len_b = (m_tot_len + m_len) << 3
-    pm_len = block_nb << 6
-    memset(m_block + m_len, 0, pm_len - m_len)
-    m_block[m_len] = 0x80
-    SHA2_UNPACK32(len_b, m_block + pm_len - 4)
-    transform(m_block, block_nb)
-    for i in 1:1:8
-        SHA2_UNPACK32(m_h[i], &digest[i << 2])
-    end
-end
-
-function update(ctx::SHA256)
+function update!(ctx::SHA256)
     message::Array{UInt8}=ctx.message
-    len::UInt32=length(message)
-    block_nb::UInt32=0
-    new_len::UInt32=rem_len::UInt32=tmp_len::UInt32=0
-    shifted_message::Array{UInt8}=zeros(0)
-    tmp_len = @SHA256BLOCKSIZE() - ctx.mlen
-    rem_len = len < tmp_len ? len : tmp_len
-    if (ctx.mlen + len < @SHA256BLOCKSIZE())
+    local len::UInt32=length(message)
+    local blocknb::UInt32=0
+    local newlen::UInt32=remlen::UInt32=tmplen::UInt32=0
+    local shiftedmessage::Array{UInt8}=zeros(0)
+    local tmplen = @sha256blocksize() - ctx.mlen
+    local remlen = len < tmplen ? len : tmplen
+    ctx.mblock[1:remlen]=message[1:remlen]
+    if (ctx.mlen + len < @sha256blocksize())
         ctx.mlen += len
         return
     end
-    new_len = len - rem_len
-    block_nb = div(new_len, @SHA256BLOCKSIZE())
-    shifted_message = ctx.message[rem_len+1:end]
-    transform(message, UInt32(1), ctx.mh)
-    transform(shifted_message, block_nb, ctx.mh)
-    rem_len = new_len % @SHA256BLOCKSIZE()
-    ctx.mblock = shifted_message
-    ctx.mlen = rem_len
-    ctx.mtotlen += (block_nb + 1) << 6
+    newlen = len - remlen
+    blocknb = div(newlen, @sha256blocksize())
+    shiftedmessage = ctx.message[remlen+1:end]
+    transform!(message, UInt32(1), ctx.mh)
+    transform!(shiftedmessage, blocknb, ctx.mh)
+    remlen = newlen % @sha256blocksize()
+    
+    local maxidx = length(shiftedmessage) > 128 ? 128 : length(shiftedmessage)
+    
+    ctx.mblock[1:maxidx] = shiftedmessage[1:maxidx]
+    ctx.mlen = remlen
+    ctx.mtotlen += (blocknb + 1) << 6
 end
 
-function compute(ctx::SHA256)
-    local digest::Array{UInt8}=zeros(UInt8, @DIGESTSIZE())
+function final!(ctx::SHA256, digest::Array{UInt8})
+    local blocknb::UInt32=0
+    local pmlen::UInt32=0
+    local lenb::UInt32=0
+    local i::Int32=0
+    blocknb = (1 + ((@sha256blocksize() - 9) < (ctx.mlen % @sha256blocksize())))
+    lenb = (ctx.mtotlen + ctx.mlen) << 3
+    pmlen = blocknb << 6
+    ctx.mblock[ctx.mlen+1:end]=zeros(UInt8, length(ctx.mblock)-ctx.mlen)
     
-    #ctx.update( (unsigned char*)input.c_str(), input.length());
-    final(ctx, digest);
- 
-    local buf::Array{UInt8}=zeros(UInt8, 2*@DIGESTSIZE()+1)
-    buf[2*@DIGESTSIZE()] = 0
-    for i in 1:1:@DIGESTSIZE()
-        sprintf(buf+i*2, "%02x", digest[i])
+    ctx.mblock[ctx.mlen+1] = 0x80
+    sha2unpack32!(lenb, sub(ctx.mblock, pmlen-3:pmlen-3))
+    transform!(ctx.mblock, blocknb, ctx.mh)
+    for i in 1:1:8
+        sha2unpack32!(ctx.mh[i], sub(digest, ((i-1) << 2)+1:((i-1) << 2)+1))
     end
+end
+
+function compute!(ctx::SHA256)
+    local digest::Array{UInt8}=zeros(UInt8, @digestsize())
+    
+    update!(ctx)
+    final!(ctx, digest)
+ 
+    local buf::Array{UInt8}=zeros(UInt8, 2*@digestsize()+1)
+    buf[2*@digestsize()] = 0
+    buf=bytes2hex(digest)
     return convert(ASCIIString, buf)
 end
 
@@ -207,17 +209,17 @@ function tests()
     #------------
     
     #pack
-    assert(SHA2_PACK32(sub(Vector{UInt8}(fraze),1:1), [UInt32(3435973836)]) == 1936682341)
+    assert(sha2pack32!(sub(Vector{UInt8}(fraze),1:1), [UInt32(3435973836)]) == 1936682341)
     #-----------
     
     #unpack
     initialZerosUnpack::Array{UInt8}=zeros(UInt8,4)
-    SHA2_UNPACK32([UInt32(2150498334)], sub(initialZerosUnpack,1:1))
+    sha2unpack32!(UInt32(2150498334), sub(initialZerosUnpack,1:1))
     assert(initialZerosUnpack == [128, 46, 0, 30])
     #-----------
     
-    assert(@SHA256K(4) == 3921009573)
-    assert(@SHA256K(34) == 773529912)
+    assert(@sha256k(4) == 3921009573)
+    assert(@sha256k(34) == 773529912)
     
     #transform
     shaInitArray::Array{UInt32}=[1779033703, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635, 1541459225]
@@ -226,29 +228,37 @@ function tests()
     transformTestFraze[1:21]=Vector{UInt8}("some test for sha2561")
     transformTestFraze[21]=128
     transformTestFraze[64]=160
-    transform(transformTestFraze, UInt32(1), shaInitArray)
+    transform!(transformTestFraze, UInt32(1), shaInitArray)
     assert(length(setdiff(shaInitArray, shaTransformResultArray)) == 0)
     #------------
     
     #update
     updateTestObject::SHA256=SHA256("some test for sha256some test for sha256some test for sha256some test for sha256")
-    update(updateTestObject)
+    update!(updateTestObject)
     assert(updateTestObject.mtotlen == 64)
     assert(updateTestObject.mlen == 16)
-    assert(length(setdiff(updateTestObject.mblock, Vector{UInt8}(" test for sha256a256some test for sha256some test for sha256some"))) == 0)
+    updateFrazeResult::Array{UInt8}=zeros(UInt8,128)
+    updateFrazeResult[1:64]=Vector{UInt8}(" test for sha256a256some test for sha256some test for sha256some")
+    assert(length(setdiff(updateTestObject.mblock, updateFrazeResult)) == 0)
     assert(length(setdiff(updateTestObject.mh, [3687268025, 3275113092, 2361836355, 1273340655, 2721684165, 1592557147, 902732290, 2085669382])) == 0)
     #------------
     
     #compute
-    
+    computeTestObject::SHA256=SHA256("some test for sha256some test for sha256some test for sha256some test for sha256")
+    assert(compute!(computeTestObject) == "7b03aec00cab2d25c4f9786dde478622197aec26912aa224e4cb04ee14072fc9")
     #------------
     
-    #final
-    
-    #------------
+    overallTest1::SHA256=SHA256("Test")
+    res1::ASCIIString=compute!(overallTest1)
+    overallTest2::SHA256=SHA256(res1)
+    res2::ASCIIString=compute!(overallTest2)
+    overallTest3::SHA256=SHA256(res2)
+    res3::ASCIIString=compute!(overallTest3)
+    assert(res1 == "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25")
+    assert(res2 == "28cac1a9a8d521b6aa3b454a19d592e5a113d08a2fbcfefb8a7b977fea140cdd")
+    assert(res3 == "3c00539acd235bde9eb4117f1fa04a6c274ee282a54cdbce88a3854022ebcc79")
 end
 tests()
 
 end
 
-workspace()
