e045289c715cd7cee13e05dd310ea09dcd39ac7e
Tomasz Rohn <tomasz.rohn@gmail.com>
2015-12-21 01:34:42 +0100

    included Enigma, md5, Serpent, and Whirpool modules in Cryptic

--- src/Cryptic.jl
+++ src/Cryptic.jl
@@ -9,6 +9,10 @@ include("RandomGenerators.jl")
 include("CipherBlocks.jl")
 include("RSA.jl")
 include("BCrypt.jl")
-export SHA2, AES, PrimeTests, RandomGenerators, CipherBlocks, RSA, BCrypt
+include("Enigma.jl")
+include("md5.jl")
+include("Serpent.jl")
+include("Whirpool.jl")
+export SHA2, AES, PrimeTests, RandomGenerators, CipherBlocks, RSA, BCrypt, enigma, whirpool, md5, serpentencrypt, serpentdecrypt
 
 end # module
