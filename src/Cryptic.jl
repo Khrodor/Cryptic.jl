4edf1815bda428996da5e8b11deb469aaa4813ea
Tomasz Rohn <tomasz.rohn@gmail.com>
2015-12-21 11:01:09 +0100

    changing include to using, so now its possible to import Cryptic and use full functionality without calling using of single module.

--- src/Cryptic.jl
+++ src/Cryptic.jl
@@ -2,17 +2,18 @@ module Cryptic
 
 # package code goes here
 
-include("SHA2.jl")
-include("AES.jl")
-include("PrimeTests.jl")
-include("RandomGenerators.jl")
-include("CipherBlocks.jl")
-include("RSA.jl")
-include("BCrypt.jl")
-include("Enigma.jl")
-include("md5.jl")
-include("Serpent.jl")
-include("Whirpool.jl")
-export SHA2, AES, PrimeTests, RandomGenerators, CipherBlocks, RSA, BCrypt, enigma, whirpool, md5, serpentencrypt, serpentdecrypt
+using SHA2
+using AES
+using PrimeTests
+using RandomGenerators
+using CipherBlocks
+using RSA
+using BCrypt
+using Enigma
+using Serpent
+using Whirpool
+using Md5
+
+export SHA2, AES, PrimeTests, RandomGenerators, CipherBlocks, RSA, BCrypt, enigma, whirpool, md5, Serpent128
 
 end # module
